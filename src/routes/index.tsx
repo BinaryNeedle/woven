2e08fdd2da412cf8a49e98995752f54ea50605fc
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2024-10-17 16:56:27 +0700

    feat: resolve git conflict
    resolve in package.json, src/routes/index.tsx and yarn.lock

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,29 @@
-import SmallChatProfile from "../components/SmallChatProfile";
+import { A } from "@solidjs/router";
 
 export default function Home() {
 	return (
-		<>
-			<aside class="max-w-[465px] bg-[#26485C]">
-				<h1 class="text-5xl">WOVEN</h1>
-				<input type="text" placeholder="Search" />
-				<SmallChatProfile />
-			</aside>
-			<main class=""></main>
-		</>
+		<main class="text-center mx-auto text-gray-700 p-4">
+			<h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+				Hello world!
+			</h1>
+			<p class="mt-8">
+				Visit{" "}
+				<a
+					href="https://solidjs.com"
+					target="_blank"
+					class="text-sky-600 hover:underline"
+				>
+					solidjs.com
+				</a>{" "}
+				to learn how to build Solid apps.
+			</p>
+			<p class="my-4">
+				<span>Home</span>
+				{" - "}
+				<A href="/about" class="text-sky-600 hover:underline">
+					About Page
+				</A>{" "}
+			</p>
+		</main>
 	);
 }
