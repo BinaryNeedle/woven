7fdaf2c59acb47af99fc056e26f06c372019a924
Ahmad Zaky <humami359@gmail.com>
2024-10-20 19:02:42 +0700

    feat: commit file for fixing git conflict

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,24 @@
 import { A } from "@solidjs/router";
+import MenuDashboard from '../components/MenuDashboard';
 
 export default function Home() {
-	return (
-		<main class="text-center mx-auto text-gray-700 p-4">
-			<h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-				Hello world!
-			</h1>
-			<p class="mt-8">
-				Visit{" "}
-				<a
-					href="https://solidjs.com"
-					target="_blank"
-					class="text-sky-600 hover:underline"
-				>
-					solidjs.com
-				</a>{" "}
-				to learn how to build Solid apps.
-			</p>
-			<p class="my-4">
-				<span>Home</span>
-				{" - "}
-				<A href="/about" class="text-sky-600 hover:underline">
-					About Page
-				</A>{" "}
-			</p>
-		</main>
-	);
+  return (
+    <main class="text-gray-700">
+      <div class="flex h-svh divide-x-2 divide-black bg-">
+        <div class="w-1/4 bg-cyan-900" id="Navigation-Pane">
+          <div class="appname justify-center items-center flex my-[39px]">
+            <img src="/assets/svg/App Title.svg" alt="" srcset="" />
+          </div>
+          <div class="searchConversation justify-center items-center flex">
+            <input type="text" name="conversation" id="conversation" class="block w-full mx-10 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-100 sm:text-sm sm:leading-6 outline-none" placeholder="Search" />
+          </div>
+        </div>
+        <div class="w-3/4 bg-cyan-900 relative" id="Conversation-Pane">
+          <div class=" p-10  bottom-0">
+            <input type="text" name="message" id="" class="rounded-md border-0 p-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-100 sm:text-sm sm:leading-6 outline-none" />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
