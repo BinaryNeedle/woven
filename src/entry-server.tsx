06b5fd6ab3418b534be6e8524273c8f88827762d
jtnqr <juliuspanjul25@gmail.com>
2024-10-25 19:42:12 +0700

    move websocket server to own file, fix solidjs http server, add some additional graphql schema definitions

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,15 +1,6 @@
 // @refresh reload
 import { createHandler, StartServer } from "@solidjs/start/server";
-import { WebSocketServer } from "ws";
-import { useServer } from "graphql-ws/lib/use/ws";
-import { schema } from "./lib/graphql"; // Your existing GraphQL schema
 
-const wsServer = new WebSocketServer({
-	port: 5000,
-	path: "/graphql",
-});
-
-useServer({ schema }, wsServer);
 
 export default createHandler(() => (
 	<StartServer
