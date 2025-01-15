58ded35d4a98eedf217ce17374570f550e96581a
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-01-16 05:47:57 +0700

    feat: fixing profile UI sidebar after merge
    UI profile sidebar from main branch sooo fixing it by using dev-shadcn branch code

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -60,19 +60,15 @@ export default function SideBar({ hostName, currentUser, otherUsers }: Props) {
 			</section>
 			<section class="grid grid-cols-4 px-8 bg-[#17394D] py-4">
 				<section class="col-span-3 flex items-center">
-					{/* <img
+					<img
 						src={`${hostName}/assets/svg/Online Status.svg`}
 						alt="Online Status"
-					/> */}
-					{/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-					</svg> */}
-
-					{/* <img
+					/>
+					<img
 						src={`${hostName}/assets/svg/Conversation Image.svg`}
 						alt="Image Profile"
 						class="mx-3"
-					/> */}
+					/>
 					{/* Displaying the username of the logged-in user */}
 					<h2 class="text-lg font-bold text-white">
 						{currentUser ? currentUser.nickname : "User  not found"}
