e9a2d5c480fd33f2525097c29df9f89aead52fc3
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-01-20 20:35:59 +0700

    feat: testing pulling data from supabase
    so far no problem

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,35 +1,46 @@
 // Kalo ada merah merah gatau gw yang penting jalan frontendnya
-import { createSignal, onMount } from "solid-js";
+import { JSXElement, Component, createResource, Suspense } from "solid-js";
+import { PrismaClient } from "@prisma/client";
 
-interface Props {
+const prisma = new PrismaClient();
+interface SideBarProps {
 	hostName: string;
-	currentUser: any;
-	otherUsers: any;
+	// currentUser: any;
+	// otherUsers: any;
 }
 
-export default function SideBar({ hostName, currentUser, otherUsers }: Props) {
+const SideBar: Component<SideBarProps> = (props): JSXElement => {
+	const [user] = createResource(async () => {
+		const response = await prisma.user.findUnique({ where: { userId: 4 } });
+		console.log(response);
+		return await response;
+	});
 	return (
-		<aside class="w-full bg-[#0E8388] flex flex-col justify-between shadow-lg h-dvh">
-			<section class="grid grid-cols-1">
-				<section class="justify-self-center my-10">
-					{/* TODO: perbaiki pemanggilan logo woven ketika tidak ditemukan */}
-					<img src={`${hostName}/assets/svg/App Title.svg`} alt="Woven Logo" />
-				</section>
-				<section class="px-5 mb-10 relative bg-[#17394D] rounded-lg input-container-text flex">
-					<input
-						placeholder="Search or type a new chat"
-						class="input-field text-slate-300 focus:text-white"
-						type="text"
-					/>
-					<label for="input-field" class="input-label"></label>
-					<span class="input-highlight"></span>
-					<img src={`${hostName}/assets/svg/search.svg`} alt="Search" />
-				</section>
-				{/* Conversation Rooms Start */}
-				<section>
-					<section id="search_result"></section>
-					<section id="conversation">
-						{otherUsers
+		<Suspense fallback={<div>Loading fetch data</div>}>
+			<aside class="w-full bg-[#0E8388] flex flex-col justify-between shadow-lg h-dvh">
+				<section class="grid grid-cols-1">
+					<section class="justify-self-center my-10">
+						{/* TODO: perbaiki pemanggilan logo woven ketika tidak ditemukan */}
+						<img
+							src={`${props.hostName}/assets/svg/App Title.svg`}
+							alt="Woven Logo"
+						/>
+					</section>
+					<section class="px-5 mb-10 relative bg-[#17394D] rounded-lg input-container-text flex">
+						<input
+							placeholder="Search or type a new chat"
+							class="input-field text-slate-300 focus:text-white"
+							type="text"
+						/>
+						<label for="input-field" class="input-label"></label>
+						<span class="input-highlight"></span>
+						<img src={`${props.hostName}/assets/svg/search.svg`} alt="Search" />
+					</section>
+					{/* Conversation Rooms Start */}
+					<section>
+						<section id="search_result"></section>
+						<section id="conversation">
+							{/* {otherUsers
 							.filter((user) => user.user_id !== currentUser.user_id) // Exclude the current user
 							.map((user) => (
 								<a
@@ -37,7 +48,7 @@ export default function SideBar({ hostName, currentUser, otherUsers }: Props) {
 									class="grid grid-cols-5 p-3 hover:cursor-pointer hover:bg-[#17394D] border-t border-[#17394D]"
 								>
 									<img
-										src={`${hostName}/assets/svg/Conversation Image.svg`}
+										src={`${props.hostName}/assets/svg/Conversation Image.svg`}
 										alt="Profile Image"
 									/>
 									<section class="col-span-3">
@@ -53,40 +64,44 @@ export default function SideBar({ hostName, currentUser, otherUsers }: Props) {
 										<p class="text-sm text-slate-300 text-end">Fri</p>
 									</section>
 								</a>
-							))}
+							))} */}
+						</section>
 					</section>
+					{/* Conversation Rooms End */}
 				</section>
-				{/* Conversation Rooms End */}
-			</section>
-			<section class="grid grid-cols-4 px-8 bg-[#17394D] py-4">
-				<section class="col-span-3 flex items-center">
-					<img
-						src={`${hostName}/assets/svg/Online Status.svg`}
-						alt="Online Status"
-					/>
-					<img
-						src={`${hostName}/assets/svg/Conversation Image.svg`}
-						alt="Image Profile"
-						class="mx-3"
-					/>
-					{/* Displaying the username of the logged-in user */}
-					<h2 class="text-lg font-bold text-white">
-						{currentUser ? currentUser.nickname : "User  not found"}
-					</h2>
+				<section class="grid grid-cols-4 px-8 bg-[#17394D] py-4">
+					<section class="col-span-3 flex items-center">
+						<img
+							src={`${props.hostName}/assets/svg/Online Status.svg`}
+							alt="Online Status"
+						/>
+						<img
+							src={`${props.hostName}/assets/svg/Conversation Image.svg`}
+							alt="Image Profile"
+							class="mx-3"
+						/>
+						{/* Displaying the username of the logged-in user */}
+						<h2 class="text-lg font-bold text-white">{user()?.username}</h2>
+					</section>
+					<a
+						href="/setting"
+						class="group mx-auto my-auto relative cursor-pointer"
+					>
+						<span>
+							<img
+								src={`${props.hostName}/assets/svg/settingsIcon.svg`}
+								alt="Menu"
+							/>
+						</span>
+						<div class="bg-[#2C3333] p-2 rounded-md group-hover:flex hidden absolute -top-2 -translate-y-full left-1/2 -translate-x-1/2">
+							<span class="text-[#CBE4DE] whitespace-nowrap">Settings</span>
+							<div class="bg-inherit rotate-45 p-1 absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2"></div>
+						</div>
+					</a>
 				</section>
-				<a
-					href="/setting"
-					class="group mx-auto my-auto relative cursor-pointer"
-				>
-					<span>
-						<img src={`${hostName}/assets/svg/settingsIcon.svg`} alt="Menu" />
-					</span>
-					<div class="bg-[#2C3333] p-2 rounded-md group-hover:flex hidden absolute -top-2 -translate-y-full left-1/2 -translate-x-1/2">
-						<span class="text-[#CBE4DE] whitespace-nowrap">Settings</span>
-						<div class="bg-inherit rotate-45 p-1 absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2"></div>
-					</div>
-				</a>
-			</section>
-		</aside>
+			</aside>
+		</Suspense>
 	);
-}
+};
+
+export default SideBar;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,71 @@
+import { createSignal, For, createResource } from "solid-js";
+import { PrismaClient } from "@prisma/client";
 import SideBar from "../components/SideBar";
 import FriendList from "../components/FriendList";
 import "../css/FriendList.css";
-
 import users from "../testing/users.json";
 
+const prisma = new PrismaClient();
+
+type User = {
+	userId: number;
+	username: String;
+	email: String;
+	nickname: String;
+	biography: String;
+	avatar_url: String;
+	last_seen: string;
+	is_online: Boolean;
+	created_at: string;
+	updated_at: string;
+};
+
 export default function Home() {
+	const [user] = createResource(async () => {
+		const response = await prisma.user.findUnique({ where: { userId: 4 } });
+		console.log(response);
+		return await response;
+	});
+	const [friends, setFriends] = createSignal([]);
 	const hostName = import.meta.env.VITE_HOSTNAME;
 	const LoggedId = 1;
-	const currentUser = users.find((user) => user.user_id === LoggedId);
-	const otherUsers = users.filter((user) => user.user_id !== LoggedId);
+	// const currentUser = async () => {
+	// 	const user = await prisma.user.findUnique({ where: { userId: 4 } });
+	// 	console.log(user);
+	// 	return user;
+	// };
+
+	// currentUser().then((responseUser) => {
+	// 	console.log(responseUser);
+	// 	setUser(responseUser);
+	// 	console.log(responseUser.username);
+	// });
+	// console.log(user);
+	// const currentUser = Promise.resolve(
+	// 	prisma.user.findUnique({
+	// 		where: { userId: 4 },
+	// 	})
+	// ).then((responseUser) => {
+	// console.log(responseUser);
+	// setUser(responseUser);
+	// console.log(responseUser.username);
+	// });
+	// console.log(user);
+	// const otherUsers = Promise.resolve(
+	// 	prisma.user_contact.findMany({ where: { owner_user_id: 4 } })
+	// ).then((user) => {
+	// 	console.log(user);
+	// 	setUser(user);
+	// });
 
 	return (
 		<main class="grid grid-cols-5 tracking-wide">
 			<SideBar
 				hostName={hostName}
-				currentUser={currentUser}
-				otherUsers={otherUsers}
+				// currentUser={currentUser.username}
+				// otherUsers={otherUsers}
 			/>
-			<FriendList
-				hostName={hostName}
-				currentUser={currentUser}
-				otherUsers={otherUsers}
-			/>
+			{/* <FriendList hostName={hostName} otherUsers={otherUsers} /> */}
 		</main>
 	);
 }
