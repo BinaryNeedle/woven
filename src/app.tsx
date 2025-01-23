d9378b670d69c538328eb7edd1a18f787ab7c6c0
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-01-23 16:26:52 +0700

    feat: adding user context
    not finish yet

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
+import { UserContextProvider } from "./context/UserContext";
 import "./css/app.css";
 
 export default function App() {
@@ -12,7 +13,9 @@ export default function App() {
 				</>
 			)}
 		>
-			<FileRoutes />
+			<UserContextProvider>
+				<FileRoutes />
+			</UserContextProvider>
 		</Router>
 	);
 }
---
+++ src/context/UserContext.tsx
@@ -0,0 +1,27 @@
+import {
+	JSXElement,
+	Component,
+	createContext,
+	createMemo,
+	ChildrenReturn,
+} from "solid-js";
+
+import { createStore } from "solid-js/store";
+
+export const UserContext = createContext();
+
+interface UserContextProps {
+	children: JSXElement;
+}
+
+export const UserContextProvider: Component<UserContextProps> = (
+	props
+): JSXElement => {
+	const [user, setUser] = createStore();
+	const userInfo = createMemo(() => user);
+	return (
+		<UserContext.Provider value={userInfo}>
+			{props.children}
+		</UserContext.Provider>
+	);
+};
